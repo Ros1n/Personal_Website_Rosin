1002c5469b48c49d769123d7ca93254d4a84b677
Xin Luo <xluo450@UoA.auckland.ac.nz>
2017-09-17 21:11:36 +1200

    first-vs-change

--- js/modules/index.js
+++ js/modules/index.js
@@ -22,3 +22,13 @@ layui.use('laydate', function(){
       ,trigger: 'click' 
 	});	
 });
+
+layui.use('element', function () {
+    var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
+
+    //监听导航点击
+    element.on('nav(demo)', function (elem) {
+        //console.log(elem)
+        layer.msg(elem.text());
+    });
+});
